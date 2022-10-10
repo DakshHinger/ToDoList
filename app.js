51c7db72e69ea2885f4d6e94b93519b48a5766c0
DakshHinger <78601608+DakshHinger@users.noreply.github.com>
2022-10-10 12:58:25 +0530

    form added

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require('express');
-const app = express();
+const bodyparser = require("body-parser");
 
+const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
+app.use(bodyparser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    let day = "thorday"; 
-    res.render('index', {day: day});
+    let day = new Date().getDay();
+    res.render('index', { day: day });
 });
+
+app.post('/', (req, res) => {
+    var item = req.body.newItem;
+    console.log(item);
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
