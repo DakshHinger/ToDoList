37e848ac63eb609c1205d60856ded67505e62c10
DakshHinger <78601608+DakshHinger@users.noreply.github.com>
2022-10-11 23:49:27 +0530

    Modify app

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const bodyparser = require("body-parser");
 const app = express();
 const port = 3000;
 
+// mongoose.connect("");
+
 var items = [];
 app.set('view engine', 'ejs');
 app.use(bodyparser.urlencoded({ extended: true }));
