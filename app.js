e745461fd825dc8954b9283760f704d19f9ec897
DakshHinger <78601608+DakshHinger@users.noreply.github.com>
2022-10-13 07:51:34 +0530

    Database Created

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const bodyparser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = 3000;
 
-// mongoose.connect("");
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+const itemsSchema = {
+    name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+    name: "This is my ToDoList"
+});
+const item2 = new Item({
+    name: "This is my ToDoList2"
+});
+const item3 = new Item({
+    name: "This is my ToDoList3"
+});
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems, (err) => {
+    if (err) {
+        console.log(err);
+    }
+})
 
 var items = [];
 app.set('view engine', 'ejs');
